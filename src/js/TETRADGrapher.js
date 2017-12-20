f1ae59c6cbc8f9304cd9cd564d79dab1bbabb32e
Ringenberg <Ringenberg@users.noreply.github.com>
2017-12-20 12:39:15 -0500

    Basic tetrad xml to cytoscape or png file tool added.

---
+++ src/js/TETRADGrapher.js
@@ -0,0 +1,170 @@
+import $ from 'jquery';
+import cytoscape from 'cytoscape';
+
+function handleFileUpload(ev) {
+    var files = ev.target.files;
+    graph.elements().remove();
+    Array.prototype.forEach.call(files, function (file) {
+        console.log(file,'is',file.type);
+        if (file.type.match('text/xml')) { // only xml files
+            console.log(file,'as xml');
+            var reader = new FileReader();
+            reader.onload = (function (xfile) {
+		return function (e) {
+		    var $xml = $($.parseXML(e.target.result));
+		    $xml.find('variable').each(function () {
+			graph.add({group: "nodes",
+				   data: {id: $(this).text()}});
+		    });
+		    $xml.find('edge').each(function () {
+			var edge = $(this).text();
+			if (edge.includes('-->')) {
+			    var st = edge.split(' --> ');
+			    graph.add({group: "edges",
+				       data: {id: $(this).text(),
+					      source: st[0],
+					      target: st[1]}});
+			}
+			if (edge.includes('o-o')) {
+			    var oo = edge.split(' o-o ');
+			    graph.add({group: "edges",
+				       data: {id: edge, source: oo[0],
+					      target: oo[1]},
+				       classes: "doublecircle"});
+			}
+			if (edge.includes('o->')) {
+			    var ol = edge.split(' o-> ');
+			    graph.add({group: "edges",
+				       data: {id: edge, source: ol[0],
+					      target: ol[1]},
+				       classes: "circlearrow"});
+			}
+			if (edge.includes('<->')) {
+			    var aa = edge.split(' <-> ');
+			    graph.add({group: "edges",
+				       data: {id: edge, source: aa[0],
+					      target: aa[1]},
+				       classes: "doublearrow"});
+			}
+		    });
+		    var layout = graph.elements().layout({
+			name:'circle',
+			padding:10,
+			radius: 125,
+			fit: false,
+			avoidOverlap: false});
+		    layout.run();
+		};
+            })(file);
+            reader.readAsText(file);
+        } else if (file.type.match('application/json')) { // or json
+            console.log(file,'as json');
+            var jreader = new FileReader();
+            jreader.onload = (function () {
+         	return function (e) {
+         	    console.log(e.target.result);
+         	    var json = JSON.parse(e.target.result);
+         	    var lopt = json.layout;
+         	    lopt.position = function(n) {
+			return {row: n.data('row') , col: n.data('column')};
+		    };
+         	    //json.elements.forEach(function(n) { console.log(n); graph.add(n); });
+         	    graph.add(json.elements);
+         	    graph.layout(lopt).run();
+         	};
+            })(file);
+            jreader.readAsText(file);
+        } else {
+            console.log(file,'as unknown');
+        }
+    });
+}
+
+var graph;
+$(function () {
+    $('#tetradxml').on('change', handleFileUpload);
+    graph = cytoscape({
+        container: $('#graph'),
+        selectionType: 'additive',
+        style: [{selector: 'node',
+                 style: {
+                     'background-color' : 'lightblue',
+                     'label' : 'data(id)',
+                     'shape' : 'roundrectangle',
+                     'text-valign' : 'center',
+                     'border-width' : '1px',
+                     'border-style' : 'solid',
+                     'border-color' : 'grey',
+                     'width': 'label',
+                     'height': 'label',
+                     'padding': 5
+                 }},
+                {selector: 'node:selected',
+                 style: { 'background-color': 'orange'}
+                },
+                {selector: 'node.latent',
+                 style: { 'shape': 'ellipse'}
+                },
+                {selector: 'edge',
+                 style: {
+		     'arrow-scale': 2,
+                     'width': 3,
+                     'line-color': 'blue',
+                     'target-arrow-shape' : 'triangle',
+                     'target-arrow-color' : 'blue',
+                     'source-arrow-color' : 'blue',
+                     'curve-style' : 'bezier'
+                 }},
+                {selector: 'edge.doublearrow',
+                 style: {
+                     'source-arrow-shape': 'triangle'
+                 }},
+                {selector: 'edge.circlearrow',
+                 style: {
+                     'source-arrow-shape': 'circle',
+                     'source-arrow-fill': 'hollow'
+                 }
+                },
+                {selector: 'edge.doublecircle',
+                 style: {
+                     'source-arrow-shape': 'circle',
+                     'target-arrow-shape': 'circle',
+                     'source-arrow-fill': 'hollow',
+                     'target-arrow-fill': 'hollow'
+                 }
+                },
+                {selector: 'edge:selected',
+                 style: {
+                     'line-color' : 'orange',
+                     'target-arrow-color' : 'orange',
+                     'source-arrow-color' : 'orange'
+                     //'line-style': 'dashed'
+                 }},
+                {selector: 'edge.undirected',
+                 style: {
+                     'target-arrow-shape': 'none',
+                     'source-arrow-shape': 'none'
+                 }} ]
+    });
+    /*graph.contextMenus(
+      {menuItems: [
+      {
+      id: 'latent',
+      title: 'Toggle Normal/Latent',
+      selector: 'node',
+      onClickFunction: function (ev) {
+      ev.cyTarget.toggleClass('latent');
+      },
+      show: true,
+      disabled: false
+      }
+      ]
+      }
+      );*/
+    $('#out_png').on('click', function () {
+        $('#image_png').attr('src', graph.png());
+    });
+    $('#out_json').on('click', function () {
+        $('#image_json').text(JSON.stringify(graph.json(), null, 2));
+    });
+});
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+
+module.exports = [{
+  bail: true,
+  entry: {
+    TETRADGrapher: ['./src/js/TETRADGrapher.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    publicPath: ''
+  },
+  target: 'web',
+  module: {
+    rules: [
+      /*{
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+        },*/
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "postcss-loader"
+        ]
+      },
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        exclude: /node_modules/,
+        loader: "jshint-loader",
+        options: {
+          esversion: 6
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackInlineSourcePlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'TETRADGrapher.html',
+      inject: 'head',
+      inlineSource: '.(js|css)$',
+      template: 'src/html/TETRADGrapher.html'
+    })
+  ]
+}]
